ce63c0b0f3fd6fdd0a977a55bc09f900b25c2a19
benhrichards <48437352+benhrichards@users.noreply.github.com>
2019-05-29 18:18:24 +0100

    Add files via upload

--- mws-restaurant-stage-1-master/js/sw.js
+++ mws-restaurant-stage-1-master/js/sw.js
@@ -7,7 +7,7 @@ let urlsToCache = [
   './js/main.js',
   './js/restaurant_info.js',
   './js/dbhelper.js',
-  './js/sw_registration.js',
+  './js/registerserviceworker.js',
   './data/restaurants.json',
   './img/1.jpg',
   './img/2.jpg',
