242d9707a732777e1c365f43f11694b1736d26f7
benhrichards <48437352+benhrichards@users.noreply.github.com>
2019-05-29 16:15:44 +0100

    Add files via upload

--- mws-restaurant-stage-1-master/js/sw.js
+++ mws-restaurant-stage-1-master/js/sw.js
@@ -1,55 +1,65 @@
-let theStatCache ='restaurant-cache-v1';
+let staticCacheName = 'restaurant-cache';
+let urlsToCache = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './js/sw_registration.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
 
+
+/**
+ * Installation of service worker
+ */
 self.addEventListener('install', function(event) {
 	event.waitUntil(
-		caches.open(theStatCache).then(function(cache){
-			return cache.all([
-				'./',
-				'./index.html',
-				'./restaurant.html',
-				'./css/stles.css',
-				'./js/dbhelper.js',
-				'./js/main.js',
-				'./js/restaurant_info.js',
-				'./js/sw.js',
-				'./data/restaurants.json',
-				'./img/1.jpg',
-				'./img/2.jpg',
-				'./img/3.jpg',
-				'./img/4.jpg',
-				'./img/5.jpg',
-				'./img/6.jpg',
-				'./img/7.jpg',
-				'./img/8.jpg',
-				'./img/9.jpg',
-				'./img/10.jpg',
-				]);
-		})
-	);
+		caches.open(staticCacheName).then(function(cache) {
+			return cache.addAll(urlsToCache);
+        })
+    );
 });
 
+
+/**
+ * Activation of service worker
+ */
 self.addEventListener('activate', function(event) {
-	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-			cacheNames.filter(function(cacheName) {
-					return cacheName.startsWith('restaurant-') &&
-						   cacheName != staticCacheName;
-				}).map(function(cacheName) {
-					return caches.delete(cacheName);
-				})
-			);
-		})
-	);
-})
-
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request)
-		.then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
+
+/**
+ * Fetching for offline content viewing
+ */
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
